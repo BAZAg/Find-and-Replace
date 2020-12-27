04053a15f0f5d0d0d6d895283777484cccaf9d4b
Юрий Йосифович <aramic2007@gmail.com>
2020-12-27 14:39:02 +0200

    Удалил лишние файлы

--- catalog/model/catalog/home_design.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ModelCatalogHomeDesign extends Model {
-	public function getDesign() {
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "home_design");
-		return $query->row;
-	}
-}
